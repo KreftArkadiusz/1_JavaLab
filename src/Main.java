742c623d1576cecdd77f6f06727107f0f4964618
Kenziexe <1areknetflix@gmail.com>
2021-11-26 14:25:19 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcjii
+//        Użyć każdej z opcjiii
     }
 }
