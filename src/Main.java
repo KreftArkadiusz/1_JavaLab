a2b789f3084df425d0b6621426acb882e7c4cb7c
Kenziexe <1areknetflix@gmail.com>
2021-11-26 14:33:43 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
-//        Użyć każdej z opcjiii
+//        Użyć każdej z opcji
+
+        System.out.println("Testowe wypisywanie w konsoli 1\n");
+
+        System.out.printf("To jest %s przykład numer %d wypisywania w konsoli\n","mój",2);
+
+        System.out.print("Wypisywanie");
+        System.out.print(" na");
+        System.out.print(" trzeci");
+        System.out.print(" sposób");
     }
 }
