c59f0ca897a14d6d3484ae90b13972ac54cf8427
Kenziexe <1areknetflix@gmail.com>
2021-11-26 16:39:13 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        double[]liczby = new double[]{11.5,15.8,5.1,55.533,12213.41234};
 
+        int wskaznik = 0;
+
+        while(wskaznik<5)
+        {
+            System.out.println(liczby[wskaznik]);
+            wskaznik++;
+        }
     }
 }
